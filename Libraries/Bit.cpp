670926f064c94b3dd3f10a610366c8972dad8037
mateusz <wykurz@gmail.com>
2014-08-31 12:09:18 -0400

    Fix yet another bug in Bit.

--- Libraries/Bit.cpp
+++ Libraries/Bit.cpp
@@ -22,9 +22,25 @@ namespace Libraries { namespace Bit {
         Count rangeCount(Index indexA_, Index indexB_) const;
 
       private:
-        Count leftCount(Index index_) const;
+        void         doAdd(Index index_, Count value_);
+        Count   singleCount(Index index_) const;
+        Count fromLeftCount(Index index_) const;
         std::vector<Index> _vector;
     };
+
+namespace {
+
+    Bit::Index toZeroBased(Bit::Index index_)
+    {
+        return index_ - 1;
+    }
+
+    Bit::Index toOneBased(Bit::Index index_)
+    {
+        return index_ + 1;
+    }
+
+}
 
     Bit::Bit(Index N)
         : _vector(N)
@@ -33,39 +49,49 @@ namespace Libraries { namespace Bit {
 
     void Bit::add(Index index_, Count value_)
     {
-        while (index_ < _vector.size())
-        {
-            _vector[index_] += value_;
-            index_ += (index_ & -index_);
-        }
+        doAdd(toOneBased(index_), value_);
     }
 
     Bit::Count Bit::count(Index index_) const
     {
-        Count sum = _vector[index_];
-        if (0 < index_) {
+        return singleCount(toOneBased(index_));
+    }
+
+    Bit::Count Bit::rangeCount(Index indexA_, Index indexB_) const
+    {
+        Count count = 0 < indexA_ ? fromLeftCount(toOneBased(indexA_ - 1)) : 0;
+        return fromLeftCount(toOneBased(indexB_)) - count;
+    }
+
+    void Bit::doAdd(Index index_, Count value_)
+    {
+        while (toZeroBased(index_) < _vector.size())
+        {
+            _vector[toZeroBased(index_)] += value_;
+            index_                       += (index_ & -index_);
+        }
+    }
+
+    Bit::Count Bit::singleCount(Index index_) const
+    {
+        Count sum = _vector[toZeroBased(index_)];
+        if (1 < index_) {
             const Index z = index_ - (index_ & -index_);
             index_--;
             while (z != index_) {
-                sum    -= _vector[index_];
+                sum    -= _vector[toZeroBased(index_)];
                 index_ -= (index_ & -index_);
             }
         }
         return sum;
     }
 
-    Bit::Count Bit::rangeCount(Index indexA_, Index indexB_) const
-    {
-        Count count = 0 < indexA_ ? leftCount(indexA_ - 1) : 0;
-        return leftCount(indexB_) - count;
-    }
-
-    Bit::Count Bit::leftCount(Index index_) const
+    Bit::Count Bit::fromLeftCount(Index index_) const
     {
         Count sum = 0;
         while (0 < index_)
         {
-            sum    += _vector[index_];
+            sum    += _vector[toZeroBased(index_)];
             index_ -= (index_ & -index_);
         }
         return sum;
@@ -121,17 +147,46 @@ namespace Libraries { namespace Bit {
         count = 1;
         bit.add(index, count);
         v[index] += count;
+        BOOST_CHECK(checkBitRange(v, bit));
 
+        index = 3;
+        count = 2;
+        bit.add(index, count);
+        v[index] += count;
+        BOOST_CHECK(checkBitRange(v, bit));
+
+        index = 3;
+        count = 2;
+        bit.add(index, count);
+        v[index] += count;
+        BOOST_CHECK(checkBitRange(v, bit));
+
+        index = 9;
+        count = 5;
+        bit.add(index, count);
+        v[index] += count;
+        BOOST_CHECK(checkBitRange(v, bit));
+
+        index = 0;
+        count = 10;
+        bit.add(index, count);
+        v[index] += count;
+        BOOST_CHECK(checkBitRange(v, bit));
+
+        index = 5;
+        count = 1000;
+        bit.add(index, count);
+        v[index] += count;
         BOOST_CHECK(checkBitRange(v, bit));
     }
 
     BOOST_AUTO_TEST_CASE(Large)
     {
-        std::size_t testSize = 1000;
+        std::size_t testSize = 100;
         std::size_t mul = 123;
         Bit bit(testSize);
         std::vector<std::size_t> v(testSize);
-        for (std::size_t i = 1; i < 100 * testSize; ++i)
+        for (std::size_t i = 1; i < 10 * testSize; ++i)
         {
             std::size_t index = (i * mul) % testSize;
             std::size_t count = i;
